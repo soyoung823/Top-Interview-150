8127daab84147d4339636545fa8d41d98ba84821
Soyoung Chung <soyoung823@gmail.com>
2025-11-11 23:27:07 -0800

    Implement isomorphic_strings_205.py

---
+++ isomorphic_strings_205.py
@@ -0,0 +1,69 @@
+"""
+205. Isomorphic Strings
+
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+
+Example 1:
+
+Input: s = "egg", t = "add"
+
+Output: true
+
+Explanation:
+
+The strings s and t can be made identical by:
+
+Mapping 'e' to 'a'.
+Mapping 'g' to 'd'.
+Example 2:
+
+Input: s = "foo", t = "bar"
+
+Output: false
+
+Explanation:
+
+The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+
+Example 3:
+
+Input: s = "paper", t = "title"
+
+Output: true
+
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.
+"""
+def isIsomorphic(self, s: str, t: str) -> bool:
+        s_hm = {}
+        t_hm = {}
+        for i in range(len(s)):
+            s_ch = s[i]
+            if s_ch not in s_hm:
+                s_hm[s_ch] = [i]
+            else:
+                s_hm[s_ch].append(i)
+            t_ch = t[i]
+            if t_ch not in t_hm:
+                t_hm[t_ch] = [i]
+            else:
+                t_hm[t_ch].append(i)
+        
+        # check each index list are matching
+        for vals in s_hm.values():
+            if vals not in t_hm.values():
+                return False
+        return True
+
+# TC: O(n), SC: O(n)
